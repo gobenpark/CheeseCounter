08137717bd60bfa228397c998b130909b7e7a0c8
bumwoo <qjadn0914@naver.com>
2018-02-27 10:03:34 +0900

    마스터 머지전

--- CheeseCounter/ViewControllers/Counter/CounterViewController.swift
+++ CheeseCounter/ViewControllers/Counter/CounterViewController.swift
@@ -11,10 +11,18 @@ import RxSwift
 import RxCocoa
 import Moya
 import XLPagerTabStrip
+import FSPagerView
 
-class NewCounterViewController: ButtonBarPagerTabStripViewController{
+class CounterViewController: ButtonBarPagerTabStripViewController{
   
   let disposeBag = DisposeBag()
+  var winModel: WinListModel?{
+    didSet{
+//      self.pagerView.reloadData()
+    }
+  }
+  
+  let pageView: BannerViewController = BannerViewController()
   
   let myPageButton: UIBarButtonItem = {
     let button = UIBarButtonItem()
@@ -37,6 +45,10 @@ class NewCounterViewController: ButtonBarPagerTabStripViewController{
     settings.style.buttonBarItemFont = UIFont.CheeseFontMedium(size: 13.8)
     settings.style.buttonBarMinimumLineSpacing = 0
     settings.style.selectedBarHeight = 1.5
+    
+    pageView.pages.append(Banner())
+    pageView.pages.append(Banner())
+    pageView.pages.append(Banner())
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -45,11 +57,38 @@ class NewCounterViewController: ButtonBarPagerTabStripViewController{
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.addChildViewController(pageView)
+    self.view.addSubview(pageView.view)
+    
+    pageView.view.snp.makeConstraints { (make) in
+      make.top.equalTo(self.buttonBarView.snp.bottom)
+      make.right.left.equalToSuperview()
+      make.height.equalTo(50)
+    }
+    
     self.navigationController?.navigationBar.setBottomBorderColor(color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), height: 1)
     
     self.view.backgroundColor = .white
     self.buttonBarView.selectedBar.backgroundColor = #colorLiteral(red: 0.9882352941, green: 0.8588235294, blue: 0.1019607843, alpha: 1)
     self.buttonBarView.backgroundColor = .white
+    
+    CheeseService.provider.rx.request(.getWinList)
+      .map(WinListModel.self)
+      .asObservable()
+      .debug()
+      .flatMap({ (model) -> Observable<[Banner]> in
+        var banners = [Banner]()
+        for data in model.result.data{
+          let banner = Banner()
+          banner.model = data
+          banners.append(banner)
+        }
+        return Observable.just(banners)
+      })
+      .subscribe(onNext:{[weak self] (banner) in
+        log.info(self)
+        self?.pageView.pages = banner
+      }).disposed(by: disposeBag)
     
     myPageButton.rx.tap
       .map {return MypageNaviViewController()}
@@ -69,9 +108,18 @@ class NewCounterViewController: ButtonBarPagerTabStripViewController{
       oldCell?.label.textColor = #colorLiteral(red: 0.6117647059, green: 0.6117647059, blue: 0.6117647059, alpha: 1)
       newCell?.label.textColor = .black
     }
+    
+    Observable.interval(3, scheduler: MainScheduler.instance)
+      .subscribe { (_) in
+        log.info("print")
+    }.disposed(by: disposeBag)
+    
+    
+    
   }
   
   override public func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
     return [GameSelectViewController(),ShopViewController()]
   }
 }
+
