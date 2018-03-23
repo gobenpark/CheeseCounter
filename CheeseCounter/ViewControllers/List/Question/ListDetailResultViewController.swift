493fe249307f4c325931b4cf3e1ba5384bd2e71f
xiilab <mintzero1109@gmail.com>
2018-03-23 11:43:05 +0900

    label 보이도록 수정

--- CheeseCounter/ViewControllers/List/Question/ListDetailResultViewController.swift
+++ CheeseCounter/ViewControllers/List/Question/ListDetailResultViewController.swift
@@ -322,14 +322,4 @@ extension ListDetailResultViewController: DZNEmptyDataSetSource{
   }
 }
 
-extension UITableView {
-  func scrollToBottom(animated: Bool) {
-    let y = contentSize.height - frame.size.height
-    setContentOffset(CGPoint(x: 0, y: (y<0) ? 0 : y), animated: animated)
-  }
-}
-
-
-
-
 
